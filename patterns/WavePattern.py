2d09185173c6229da60aa358b21677fee89ded3f
Adam Ringwood <adam@nexadmin.com>
2017-12-31 16:44:04 -0600

    Chaos Pattern

--- patterns/WavePattern.py
+++ patterns/WavePattern.py
@@ -27,6 +27,30 @@ class WavePattern(SamplePattern):
     def teardown(self):
         pass
 
+class ChoasWavePattern(SamplePattern):
+
+    def setup(self, size=(6,6,12)):
+        super().setup()
+        self.pixels = list(map(lambda x : hsv_to_rgb(random.randint(0, 100) * 0.02, 0.5, 0.3), range(self.max_elements)))
+
+        self.i = 0
+
+    def tick(self):
+        results = []
+
+        for i in self.pixels:
+            r = i[0] + random.randint(-3, 3) * 0.02
+            b = min(i[2] + 0.0002, 1.0)
+            results.append((r,i[1],b))
+
+        self.pixels = results
+
+        output = list(map(lambda y : [x * 255.0 for x in (y[0], y[1], y[2])], self.pixels))
+        return output
+
+    def teardown(self):
+        pass
+
 class BroadWavePattern(SamplePattern):
 
     def setup(self, size=(6,6,12)):
