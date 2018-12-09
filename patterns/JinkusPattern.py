c3ae3657191c827ebe1894a0275f5bd7b0dd6b1b
Ethan <ethan>
2018-12-08 22:12:04 -0600

    Add masking helper function to jinkus

--- patterns/JinkusPattern.py
+++ patterns/JinkusPattern.py
@@ -21,6 +21,9 @@ def get_flat_index(x, y, z):
         return 0
 
 def tuplemanip(op, t_a, t_b=None):
+    # Manipulate tuples with 'op' operation
+    # e.g., tuplemanip(operator.add, (1,2,3), (4,5,6))
+    # e.g., tuplemanip(sign, (1,2,3))
     if t_b != None:
         return tuple(map(op, t_a, t_b))
     else:
@@ -38,15 +41,10 @@ def cube(pointA, pointB):
     deltas = tuplemanip(operator.sub, pointA, pointB)
     delta_signs = tuplemanip(sign, deltas)
 
-    # print(deltas)
-    # print(delta_signs)
-
     for i in range(0, deltas[0]+delta_signs[0], delta_signs[0]):
         for j in range(0, deltas[1]+delta_signs[1], delta_signs[1]):
             for k in range(0, deltas[2]+delta_signs[2], delta_signs[2]):
                 points.append(tuplemanip(operator.sub, pointA, (i,j,k)))
-
-    # print(points)
 
     return points
 
@@ -58,31 +56,64 @@ def add_pts(pattern, points, color):
 
     return pattern
 
+def mask(pattern, mask):
+    # Remove all 'mask' points from 'pattern'
+    for i in range(SIZE[0]):
+        for j in range(SIZE[1]):
+            for k in range(SIZE[2]):
+                if (i,j,k) not in mask:
+                    flat_index = get_flat_index(*(i,j,k))
+                    pattern[flat_index] = (0,0,0)
+
+    return pattern
+
+def christmas_fire_pattern():
+    pattern = []
+    for i in range(36):
+        intensity = random.random()
+        pattern += gen_random_string(intensity*.15+.5)
+    return pattern
+
+
+def gen_random_string(intensity):
+    n1 = random.randint(2,7)
+    n2 = random.randint(4,12-n1)
+    string = (
+        [(0,0,0) for i in range(n1)] +
+        [(0,intensity*255,0)]*(n2) + 
+        [(intensity*255, intensity*220, intensity*200)] * (12-n1-n2)
+        )
+
+    # print(len(string), n, "\n")
+    return string
+
+
 class JinkusPattern(SamplePattern):
 
     def setup(self, size=SIZE):
         super().setup()
         self.i = 0
         self.counter = 0
-        self.period=10
+        self.period = 200
         self.last_pattern = [(0,0,0)] * self.max_elements
 
     def tick(self):
         if (self.i % self.period) == 0:
-            # pattern = []
-            # for i in range(36):
-            #     intensity = random.random()
-            #     pattern += self.gen_random_string(intensity*.4+.2)
-            # self.last_pattern = pattern
-
-
+            # Blank out pattern
             pattern = results = [(0,0,0)] * self.max_elements
+
+            # Add christmas fire pattern
+            cfp = christmas_fire_pattern()
+            pattern = cfp
+
+            # Create tree pattern
+            tree_pts = cube((2,2,2), (3,3,3)) + cube((1,1,4), (4,4,7)) + cube((0,0,8), (5,5,11))
+            
+            # Apply tree pattern as mask
+            pattern = mask(pattern, tree_pts)
+
+            # Add star pattern
             pattern = add_pts(pattern, cube((2,2,1), (3,3,1)), (255,255,0))
-            pattern = add_pts(pattern, cube((2,2,2), (3,3,3)), (200,0,0))
-            pattern = add_pts(pattern, cube((1,1,4), (4,4,7)), (200,0,0))
-            pattern = add_pts(pattern, cube((0,0,8), (5,5,11)), (200,0,0))
-            # pattern = add_pts(pattern, cube((2,2,4), (3,3,8)), (255,0,0))
-            # pattern = add_pts(pattern, cube((1,1,9), (4,4,11)), (255,0,0))
 
             self.counter+=1
             self.last_pattern = pattern
@@ -112,15 +143,3 @@ class JinkusPattern(SamplePattern):
         cube = plane*6
 
         return cube
-
-    def gen_random_string(self, intensity):
-        n1 = random.randint(2,7)
-        n2 = random.randint(4,12-n1)
-        string = (
-            [(0,0,0) for i in range(n1)] +
-            [(0,intensity*255,0)]*(n2) + 
-            [(intensity*255, intensity*220, intensity*200)] * (12-n1-n2)
-            )
-
-        # print(len(string), n, "\n")
-        return string
