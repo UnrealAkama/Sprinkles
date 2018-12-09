4f2a927384305096fc7ed39703a589086f6206dc
Ethan <ethan>
2018-12-08 21:51:35 -0600

    Add helper functions to jinkus

--- patterns/JinkusPattern.py
+++ patterns/JinkusPattern.py
@@ -1,30 +1,90 @@
 from sample_pattern import SamplePattern
+import operator
 import random
+
+SIZE = (6,6,12)
+
+def get_flat_index(x, y, z):
+    # Remap from hardware to standard XYZ coordinates
+    # After remapping:
+    #   X is left-to-right
+    #   Y is front-to-back
+    #   Z is up-to-down
+
+    x_mapped = z
+    y_mapped = y
+    z_mapped = x
+
+    if  (0<=x and x<SIZE[0]) and (0<=y and y<SIZE[1]) and (0<=z and z<SIZE[2]):
+        return x_mapped + (SIZE[2])*y_mapped + (SIZE[1]*SIZE[2])*z_mapped
+    else:
+        return 0
+
+def tuplemanip(op, t_a, t_b=None):
+    if t_b != None:
+        return tuple(map(op, t_a, t_b))
+    else:
+        return tuple(map(op, t_a))
+
+def sign(i):
+    if i == 0: return 1
+    return int(i/abs(i))
+
+def cube(pointA, pointB):
+    # Return a list of points that fall within the volume of the cube
+    # Computation is inclusive of endpoints
+    points = []
+
+    deltas = tuplemanip(operator.sub, pointA, pointB)
+    delta_signs = tuplemanip(sign, deltas)
+
+    # print(deltas)
+    # print(delta_signs)
+
+    for i in range(0, deltas[0]+delta_signs[0], delta_signs[0]):
+        for j in range(0, deltas[1]+delta_signs[1], delta_signs[1]):
+            for k in range(0, deltas[2]+delta_signs[2], delta_signs[2]):
+                points.append(tuplemanip(operator.sub, pointA, (i,j,k)))
+
+    # print(points)
+
+    return points
+
+def add_pts(pattern, points, color):
+    # Add 'points' of 'color' to the pattern (6x6x12 input)
+    for i in points:
+        flat_index = get_flat_index(*i)
+        pattern[flat_index] = color
+
+    return pattern
 
 class JinkusPattern(SamplePattern):
 
-    def setup(self, size=(6,6,12)):
+    def setup(self, size=SIZE):
         super().setup()
         self.i = 0
-        self.period=200
+        self.counter = 0
+        self.period=10
         self.last_pattern = [(0,0,0)] * self.max_elements
 
     def tick(self):
-        # if (self.i % self.period) == 0:
-        #     pattern = self.gen_pattern_green(self.i/8.0)
-        #     self.last_pattern = pattern
-        #     return pattern
-        # else:
-        #     pattern = self.gen_pattern_red(self.i/8.0)
-        #     self.last_pattern = pattern
-        #     return pattern
-        #     return self.last_pattern
-
         if (self.i % self.period) == 0:
-            pattern = []
-            for i in range(36):
-                intensity = random.random()
-                pattern += self.gen_random_string(intensity*.2+.5)
+            # pattern = []
+            # for i in range(36):
+            #     intensity = random.random()
+            #     pattern += self.gen_random_string(intensity*.4+.2)
+            # self.last_pattern = pattern
+
+
+            pattern = results = [(0,0,0)] * self.max_elements
+            pattern = add_pts(pattern, cube((2,2,1), (3,3,1)), (255,255,0))
+            pattern = add_pts(pattern, cube((2,2,2), (3,3,3)), (200,0,0))
+            pattern = add_pts(pattern, cube((1,1,4), (4,4,7)), (200,0,0))
+            pattern = add_pts(pattern, cube((0,0,8), (5,5,11)), (200,0,0))
+            # pattern = add_pts(pattern, cube((2,2,4), (3,3,8)), (255,0,0))
+            # pattern = add_pts(pattern, cube((1,1,9), (4,4,11)), (255,0,0))
+
+            self.counter+=1
             self.last_pattern = pattern
         else:
             pattern = self.last_pattern
@@ -64,16 +124,3 @@ class JinkusPattern(SamplePattern):
 
         # print(len(string), n, "\n")
         return string
-
-class PinkusPattern(JinkusPattern):
-
-    def setup(self, size=(6,6,12)):
-        super().setup()
-
-        self.change = -1
-
-    def tick(self):
-        return super().tick()
-
-    def teardown(self):
-        super().teardown()
